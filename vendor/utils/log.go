d97217532074416feaa88eb383d32a2e3f59c822
liuzhenlin <liuzhenlin@sensefun.com>
2017-11-21 18:38:52 +0800

    修改log

--- vendor/utils/log.go
+++ vendor/utils/log.go
@@ -7,7 +7,7 @@ import (
 
 var Log = &Mlog{}
 
-type Mlog struct {}
+type Mlog struct{}
 
 func (log *Mlog) Error2Exit(err error, msg string) {
 	if err != nil {
@@ -16,18 +16,20 @@ func (log *Mlog) Error2Exit(err error, msg string) {
 	}
 }
 
-func (log *Mlog)Info(msg string)  {
+func (log *Mlog) Info(msg string) {
 	fmt.Printf("log info: %s\n", msg)
 }
 
-func (log *Mlog)Warn(msg string)  {
+func (log *Mlog) Warn(msg string) {
 	fmt.Printf("log warning: %s\n", msg)
 }
 
-func (log *Mlog)Debug(msg string)  {
+func (log *Mlog) Debug(msg string) {
 	fmt.Printf("log debug: %s\n", msg)
 }
 
-func (log *Mlog)Error(msg string)  {
-	fmt.Printf("log error: %s\n", msg)
+func (log *Mlog) Error(err error, msg string) {
+	if err != nil {
+		fmt.Printf("%s: %s\n", msg, err)
+	}
 }
